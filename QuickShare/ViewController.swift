5efa0f0ac913e9d23c7105f4814a446e164f6e4c
Alex Slatinin <sandroisu1411@gmail.com>
2025-10-18 22:31:41 +0300

    Initial Commit

---
+++ QuickShare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickShare
+//
+//  Created by Александр Слатинин on 18/10/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
